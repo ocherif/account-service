be1b018d2cefc9a7e727619498bb26b826b38b0b
ocherif <oussama.cherif1986@gmail.com>
2019-06-26 01:55:42 +0200

    follow up

--- src/main/java/com/services/account/AccountApplication.java
+++ src/main/java/com/services/account/AccountApplication.java
@@ -2,11 +2,14 @@ package com.services.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AccountApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(AccountApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 }
